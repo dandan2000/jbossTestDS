8125f328787013854abc20a48a24254defae2c58
Daniel Celentano <dacelent@redhat.com>
2022-12-14 13:10:51 -0300

    source code

---
+++ src/main/java/com/dandan/testds/Gofordata.java
@@ -0,0 +1,157 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.dandan.testds;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author dacelent
+ */
+public class Gofordata extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, NamingException, SQLException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        DataSource ds = null;
+        try {
+            Context ctx = new InitialContext();
+            ds = (DataSource) ctx.lookup("java:/MSSQLDS2");
+        } catch (NamingException e) {
+            System.out.println("Naming Exception: " + e);
+        }
+
+        if (ds == null) {
+            throw new IllegalArgumentException("No Datasource could be found");
+        }
+
+        PrintWriter out = response.getWriter();
+        try {
+
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Gofordata</title>");
+
+            Connection connection = null;
+            ResultSet resultSet = null;
+            PreparedStatement preparedStatement = null;
+
+            try {
+                out.println("DS" + ds.getConnection().getClass());
+                connection = ds.getConnection();
+            } catch (SQLException ex) {
+                Logger.getLogger(Gofordata.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            String query = "SELECT * FROM Inventory";
+            preparedStatement = connection.prepareStatement(query);
+            resultSet = preparedStatement.executeQuery();
+
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Gofordata at " + request.getContextPath() + "</h1>");
+
+            out.println("</br><table cellspacing='0' cellpadding='5' border='1'>");
+            out.println("<tr>");
+            out.println("<td><b>#</b></td>");
+            out.println("<td><b>Name</b></td>");
+            out.println("</tr>");
+
+            while (resultSet.next()) {
+                out.println("<tr>");
+                out.println("<td>" + resultSet.getString(1) + "</td>");
+                out.println("<td>" + resultSet.getString(2) + "</td>");
+                out.println("</tr>");
+
+            }
+
+            out.println("</table></br>");
+
+            out.println("</body>");
+            out.println("</html>");
+        } finally {
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (NamingException ex) {
+            Logger.getLogger(Gofordata.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(Gofordata.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (NamingException ex) {
+            Logger.getLogger(Gofordata.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(Gofordata.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
